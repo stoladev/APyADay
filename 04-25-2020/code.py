53a9075ee36d15d69d8092843cac674983d3c639
stoladev <mike@stoladev.com>
2020-04-25 21:51:09 -0500

    Added code sample of new dictionary variable. Pretty straight forward - will be interesting to see capabilities and use-case scenario.

---
+++ 04-25-2020/code.py
@@ -0,0 +1,9 @@
+# Dictionary example
+animals_in_zoo = {}
+
+# Adding items into the dictionary
+animals_in_zoo["zebras"] = 8
+animals_in_zoo["monkeys"] = 12
+animals_in_zoo["dinosaurs"] = 0
+
+print(animals_in_zoo)
