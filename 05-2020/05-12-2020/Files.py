e801363a0d7973232d365ce985f96756acf93f2f
Michael Stolarz <mike@stoladev.com>
2020-05-12 22:14:12 -0500

    Opening and closing CSV files without translation.

---
+++ 05-2020/05-12-2020/Files.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# CSV Files
+#
+# CSV files are an example of a text file that impose a structure toheir data.
+# CSV stands for Comma-Separated Values and are usually the way that data
+# from spreadsheet software is exported into a portable format.
+#
+# CSV files are just plain text files!
