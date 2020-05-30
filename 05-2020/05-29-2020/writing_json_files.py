5e8fdf487a86a9ba00737e4eb4004853879ac1ad
Michael Stolarz <mike@stoladev.com>
2020-05-29 22:34:53 -0500

    Last file-specific lesson for Python

---
+++ 05-2020/05-29-2020/writing_json_files.py
@@ -0,0 +1,24 @@
+"""
+Translating Python objects to JSON
+
+This is the last lesson regarding files for a while.
+
+Lessons learned:
+- Open up file objects using open() and with.
+- Read a file’s full contents using Python’s .read() method.
+- Read a file line-by-line using .readline() and .readlines()
+- Create new files by opening them in write-mode.
+- Append to a file non-destructively by opening a file in append-mode.
+"""
+
+import json
+
+data_payload = [{
+    'interesting message':
+    'What is JSON? A web application\'s little pile of secrets.',
+    'follow up': 'But enough talk!'
+}]
+
+with open('data.json', 'w') as data_json:
+    json.dump(data_payload, data_json)
+
