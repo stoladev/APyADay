6995e60fa4a2e8521c8ea8c580d0107a738e355a
Michael Stolarz <mike@stoladev.com>
2020-06-03 22:44:29 -0500

    Initial commit

---
+++ 06-2020/06-02-2020/python_game.py
@@ -0,0 +1,11 @@
+"""
+Main file for first try at developing a Python game.
+"""
+
+from decimal import Decimal
+
+
+def game():
+    """Main game function."""
+    test = 5 + Decimal("5")
+    print(test)
