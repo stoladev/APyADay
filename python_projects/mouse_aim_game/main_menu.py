04ee2ad31c1db402904eabb2a3e7c9d2ec90d71f
Michael Stolarz <mike@stoladev.com>
2020-06-12 14:42:05 -0500

    Added settings/credits buttons to the main menu.

--- python_projects/mouse_aim_game/main_menu.py
+++ python_projects/mouse_aim_game/main_menu.py
@@ -1,24 +1,51 @@
 """ Handles the main menu of the game. """
 
+import pygame
+
 import ui_element
 import settings
-import pygame
 
 screen = settings.screen
 
 
-def ui_load():
+def main_menu_ui_load():
     """Loads all UI elements.
     """
 
+    start_button_x_pos = settings.game_width / 2
+    start_button_y_pos = settings.game_height / 2.5
+
     start_button = ui_element.UIElement(
-        center_position=(settings.game_width / 2, settings.game_height / 2),
+        center_position=(start_button_x_pos, start_button_y_pos),
         font_size=50,
-        bg_rgb=None,
         text_rgb=settings.WHITE,
         text="CLICK TO START",
     )
 
+    credits_button_x_pos = settings.game_width / 2
+    credits_button_y_pos = start_button_y_pos * 2
+
+    credits_button = ui_element.UIElement(
+        center_position=(credits_button_x_pos, credits_button_y_pos),
+        font_size=20,
+        text_rgb=settings.WHITE,
+        text="CREDITS",
+    )
+
+    settings_button_x_pos = settings.game_width / 2
+    settings_button_y_pos = credits_button_y_pos / 0.9
+
+    settings_button = ui_element.UIElement(
+        center_position=(settings_button_x_pos, settings_button_y_pos),
+        font_size=20,
+        text_rgb=settings.WHITE,
+        text="SETTINGS",
+    )
+
     screen.fill((settings.MAIN_BG_COLOR))
-    start_button.update(pygame.mouse.get_pos())
+    start_button.ui_hover_check(pygame.mouse.get_pos())
+    credits_button.ui_hover_check(pygame.mouse.get_pos())
+    settings_button.ui_hover_check(pygame.mouse.get_pos())
     start_button.draw(screen)
+    credits_button.draw(screen)
+    settings_button.draw(screen)
