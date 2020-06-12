c959b51800c7e37537c7f93cc82b6ab667bf8c1b
Michael Stolarz <mike@stoladev.com>
2020-06-11 23:59:08 -0500

    Organized loads for settings and UI elements.

--- python_projects/mouse_aim_game/main_menu.py
+++ python_projects/mouse_aim_game/main_menu.py
@@ -2,11 +2,23 @@
 
 import ui_element
 import settings
+import pygame
 
-uielement = ui_element.UIElement(
-    center_position=(settings.game_width / 2, settings.game_height / 2),
-    font_size=50,
-    bg_rgb=None,
-    text_rgb=settings.WHITE,
-    text="CLICK TO START",
-)
+screen = settings.screen
+
+
+def ui_load():
+    """Loads all UI elements.
+    """
+
+    start_button = ui_element.UIElement(
+        center_position=(settings.game_width / 2, settings.game_height / 2),
+        font_size=50,
+        bg_rgb=None,
+        text_rgb=settings.WHITE,
+        text="CLICK TO START",
+    )
+
+    screen.fill((settings.MAIN_BG_COLOR))
+    start_button.update(pygame.mouse.get_pos())
+    start_button.draw(screen)
