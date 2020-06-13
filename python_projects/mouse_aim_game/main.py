efe1c749f6df9a743c3c030b7d0dee4c85b42560
Michael Stolarz <mike@stoladev.com>
2020-06-12 21:11:14 -0500

    Finished moving all actions to their own inits.

--- python_projects/mouse_aim_game/main.py
+++ python_projects/mouse_aim_game/main.py
@@ -2,6 +2,7 @@
 Game concept: mouse aim practice game. Created with Pygame.
 """
 
+# TODO Fix the target reloading on click
 # TODO Create seperate todo list file for the game
 # TODO Create seperate input_handler class
 # TODO Transfer Target into Sprite group
@@ -11,37 +12,12 @@ Game concept: mouse aim practice game. Created with Pygame.
 
 import pygame
 
-import target
 import main_menu
 import settings
 
 pygame.init()
-settings.cursor_init()
+settings.init()
 
-IS_RUNNING = True
-title = pygame.display.set_caption("Mouse Aim Practice")
-target = target.Target()
-clock = pygame.time.Clock()
-screen = settings.screen
-
-while IS_RUNNING:
-
-    for event in pygame.event.get():
-
-        if event.type == pygame.QUIT:
-            IS_RUNNING = False
-
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            target.handle_input(mouse_state="DOWN")
-
-    main_menu.main_menu_ui_load()
-
-    # if playing_game:
-    # target.draw(screen)
-
-    pygame.display.update()
-
-    # FPS
-    clock.tick(60)
+main_menu.init()
 
 pygame.quit()
