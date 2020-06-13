b9eeda34596ab7ca3a0686b3f3f6582fdb78d648
Michael Stolarz <mike@stoladev.com>
2020-06-12 21:12:00 -0500

    Fully functioning menu system.

--- python_projects/mouse_aim_game/main_menu.py
+++ python_projects/mouse_aim_game/main_menu.py
@@ -8,13 +8,37 @@ import settings
 screen = settings.screen
 
 
-def main_menu_ui_load():
+def init():
+    """
+    Draws the main menu screen.
+    """
+    in_main_menu = True
+    mouse_state = None
+    while in_main_menu:
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                in_main_menu = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_state = "DOWN"
+
+        ui_elements_load(mouse_state)
+        mouse_state = None
+
+        pygame.display.update()
+        settings.clock.tick(60)
+
+
+def ui_elements_load(mouse_state):
     """Loads all UI elements.
     """
 
     start_button_x_pos = settings.game_width / 2
     start_button_y_pos = settings.game_height / 2.5
 
+    # START BUTTON
     start_button = ui_element.UIElement(
         center_position=(start_button_x_pos, start_button_y_pos),
         font_size=50,
@@ -25,6 +49,7 @@ def main_menu_ui_load():
     credits_button_x_pos = settings.game_width / 2
     credits_button_y_pos = start_button_y_pos * 2
 
+    # CREDITS BUTTON
     credits_button = ui_element.UIElement(
         center_position=(credits_button_x_pos, credits_button_y_pos),
         font_size=20,
@@ -35,6 +60,7 @@ def main_menu_ui_load():
     settings_button_x_pos = settings.game_width / 2
     settings_button_y_pos = credits_button_y_pos / 0.9
 
+    # SETTINGS BUTTON
     settings_button = ui_element.UIElement(
         center_position=(settings_button_x_pos, settings_button_y_pos),
         font_size=20,
@@ -43,9 +69,12 @@ def main_menu_ui_load():
     )
 
     screen.fill((settings.MAIN_BG_COLOR))
-    start_button.ui_hover_check(pygame.mouse.get_pos())
-    credits_button.ui_hover_check(pygame.mouse.get_pos())
-    settings_button.ui_hover_check(pygame.mouse.get_pos())
+
+    mouse_pos = pygame.mouse.get_pos()
+    start_button.ui_interactions(mouse_pos, mouse_state)
+    credits_button.ui_interactions(mouse_pos, mouse_state)
+    settings_button.ui_interactions(mouse_pos, mouse_state)
+
     start_button.draw(screen)
     credits_button.draw(screen)
     settings_button.draw(screen)
