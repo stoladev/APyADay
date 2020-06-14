59eb490989ebdeefa2eedeb434e0647a3fd65616
Michael Stolarz <mike@stoladev.com>
2020-06-13 19:26:39 -0500

    Moved all TODO items to a seperate file for simpler function using vimwiki.

---
+++ python_projects/mouse_aim_game/TODOs.vw
@@ -0,0 +1,9 @@
+
+== Main Objectives ==
+- [ ] Fix the target reloading on click
+- [ ] Create seperate todo list file for the game
+- [ ] Create seperate input_handler class
+- [ ] Transfer Target into Sprite group
+- [ ] Countdown timer to main game (3... 2... 1... GO!)
+- [ ] Leaderboard during countdown
+- [ ] Random target spawning
--- python_projects/mouse_aim_game/main.py
+++ python_projects/mouse_aim_game/main.py
@@ -2,14 +2,6 @@
 Game concept: mouse aim practice game. Created with Pygame.
 """
 
-# TODO Fix the target reloading on click
-# TODO Create seperate todo list file for the game
-# TODO Create seperate input_handler class
-# TODO Transfer Target into Sprite group
-# TODO Countdown timer to main game (3... 2... 1... GO!)
-# TODO Leaderboard during countdown
-# TODO Random target spawning
-
 import pygame
 
 import main_menu
