c7362b474d62ad2cd9b73df7b457347b50ffc992
Michael Stolarz <mike@stoladev.com>
2020-06-15 14:06:52 -0500

    Finally got no-lag player movement.

--- python_projects/mouse_aim_game/player.py
+++ python_projects/mouse_aim_game/player.py
@@ -3,8 +3,6 @@
 import pygame
 from pygame.sprite import Sprite
 
-# all_sprites = pygame.sprite.Group()
-
 
 class Player(Sprite):
     """
@@ -12,13 +10,30 @@ class Player(Sprite):
     location, health, size, bank, and items.
     """
 
+    walking_up = False
+    walking_down = False
+    walking_left = False
+    walking_right = False
+
     def __init__(self):
 
         Sprite.__init__(self)
 
         self.image = pygame.image.load("./img/player.png").convert_alpha()
         self.rect = self.image.get_rect()
-        self.rect.center = 500, 500
+        self.rect.center = 50, 50
 
-    # def update(self):
+    def update(self):
+        """
+        Updates the Player sprite.
+        """
 
+        # MOVEMENT
+        if self.walking_up:
+            self.rect.y -= 5
+        if self.walking_down:
+            self.rect.y += 5
+        if self.walking_left:
+            self.rect.x -= 5
+        if self.walking_right:
+            self.rect.x += 5
