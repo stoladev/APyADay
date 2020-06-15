bec70c76ff45a805298f185d8bbc11fc02556b17
Michael Stolarz <mike@stoladev.com>
2020-06-15 14:07:31 -0500

    No-lag target updating on player click.

--- python_projects/mouse_aim_game/target_game.py
+++ python_projects/mouse_aim_game/target_game.py
@@ -24,3 +24,17 @@ class Target(Sprite):
         self.image = pygame.image.load("./img/target.png").convert_alpha()
         self.rect = self.image.get_rect()
         self.rect.center = (random_x_pos, random_y_pos)
+
+    def update(self):
+        """
+        Updates the Target sprite.
+        """
+        mouse_pos = pygame.mouse.get_pos()
+        mouse_on_element = self.rect.collidepoint(mouse_pos)
+
+        new_x_pos = random.randint(100, game_width - 100)
+        new_y_pos = random.randint(100, game_height - 100)
+
+        if mouse_on_element:
+            if settings.CLICKING:
+                self.rect.center = (new_x_pos, new_y_pos)
