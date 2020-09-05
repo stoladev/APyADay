34c35216e3f7c053f59fcc4ef61aef4321b31152
Michael Stolarz <mike@stoladev.com>
2020-09-04 21:23:37 -0500

    Added a multiple line log print method for easier data checks.

--- machine-learning/virtual_assistant/utils/debug.py
+++ machine-learning/virtual_assistant/utils/debug.py
@@ -4,3 +4,18 @@ activated = None
 def log(message):
     if activated:
         print(message)
+
+
+def log_lines(file_to_print, quantity=10):
+    """
+    Prints x (default is 10) lines from the provided file.
+
+    :param file_to_print: The file used to grab and print lines from.
+    :param quantity: The quantity of lines to print out (default is 10).
+    """
+
+    global datafile
+    with open(file_to_print, "rb") as datafile:
+        lines = datafile.readlines()
+    for line in lines[:quantity]:
+        print(line)
