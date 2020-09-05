29d4d50e7d6f3872d70f7f2e224b74d19ca8acc3
Michael Stolarz <mike@stoladev.com>
2020-09-04 21:22:00 -0500

    Added docstring; cmd line conversing only for now. Will re-add tts later.

--- machine-learning/virtual_assistant/modules/handlers/thread_handler.py
+++ machine-learning/virtual_assistant/modules/handlers/thread_handler.py
@@ -1,19 +1,27 @@
-from playsound import playsound
-from modules.listeners.microphone_listener import microphone_input
-from modules.listeners.keyboard_listener import keyboard_input
 import concurrent.futures
 import os
+
+from modules.rnn.rnn_tools.rnn_evaluator import converse
 
 threads = []
 
 
 def run_threads():
+    """
+    Runs the main threads for the program.
+
+    Threads:
+    Starting welcome audio
+    Microphone listener
+    Keyboard listener
+    """
+
     with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-        threads.append(executor.submit(playsound, "audio/start_audio.mp3"))
-        threads.append(executor.submit(microphone_input))
-        threads.append(executor.submit(keyboard_input))
+        converse()
+        # threads.append(executor.submit(playsound, "audio/start_audio.mp3"))
+        # threads.append(executor.submit(microphone_input))
+        # threads.append(executor.submit(keyboard_input))
 
 
 def stop_threads():
-    print("Exiting program...")
     os._exit(0)
