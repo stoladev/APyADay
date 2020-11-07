8eee4dfc3267e108737642601069582404235a85
Michael Stolarz <mike@stoladev.com>
2020-11-07 12:32:51 -0600

    File docstring created.

--- gui-projects/bug_tracker/technician_client/modules/loaders/mongodb_loader.py
+++ gui-projects/bug_tracker/technician_client/modules/loaders/mongodb_loader.py
@@ -1,3 +1,7 @@
+"""
+Loads the mongodb login information to connect to the necessary cluster.
+"""
+
 from pymongo import MongoClient
 
 connection = open("../modules/loaders/mongo_cluster.txt", "r")
