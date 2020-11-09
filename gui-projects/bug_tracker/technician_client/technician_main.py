c86a45024d9ba6fec311f0431f3151eedb2db36c
Michael Stolarz <mike@stoladev.com>
2020-11-09 17:26:30 -0600

    Fixed a small issue where exiting login window on technician's app didn't close the process.

--- gui-projects/bug_tracker/technician_client/technician_main.py
+++ gui-projects/bug_tracker/technician_client/technician_main.py
@@ -10,30 +10,31 @@ from technician_client.technician_login_window import TechnicianLoginWindow
 from technician_client.technician_window import TechnicianMainWindow
 
 
-def run_login_window():
+def run_login_window(app_in_use):
     """
     Runs the login verification process through the technician's login window class.
 
     :return: The main technician window if login verification is successful. Otherwise, cancels.
     """
-    app = QApplication(sys.argv)
 
     technician_login_window = TechnicianLoginWindow()
 
     if technician_login_window.exec_() == QDialog.Accepted:
-        run_main_window()
+        run_main_window(app_in_use)
+    else:
+        return
 
-    sys.exit(app.exec_())
+    # sys.exit(app_in_use.exec_())
 
 
-def run_main_window():
+def run_main_window(app_in_use):
     """
     Runs the main technician client through the technician's main window class.
 
     :return: All data associated with the MongoDB connected to.
     """
-    app = QApplication(sys.argv)
 
+    print("loaded")
     technician_main_window = TechnicianMainWindow()
     technician_main_window.show()
 
@@ -41,5 +42,7 @@ def run_main_window():
 
 
 if __name__ == "__main__":
-    run_login_window()
+    app = QApplication(sys.argv)
+    run_login_window(app)
+    # app.quitOnLastWindowClosed()
     # run_main_window()
