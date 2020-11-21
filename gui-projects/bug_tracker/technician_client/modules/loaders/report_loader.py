7c39a8ce0cbe676d68022d03713f043b2b4f4ee5
Michael Stolarz <mike@stoladev.com>
2020-11-21 14:42:28 -0600

    Initial commit.

---
+++ gui-projects/bug_tracker/technician_client/modules/loaders/report_loader.py
@@ -0,0 +1,37 @@
+"""
+Loads all data regarding reports.
+"""
+
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
+
+
+def load_reports(window):
+    """
+    Loads/reloads the accounts for the accounts table, grabbing the necessary data from MongoDB.
+
+    :param window: The QMainWindow in use.
+    :return: Returns all existing accounts with their current data.
+    """
+    table: QTableWidget = window.reports_table
+
+    selection = table.selectionModel().currentIndex()
+    table.clearSelection()
+
+    table.setRowCount(0)
+
+    collection = window.database.reports
+
+    reports = collection.find({})
+
+    i = 0
+    for document in reports:
+        submitter = document["account_name"] + " (" + document["employee_type"] + ")"
+        table.insertRow(i)
+        table.setItem(i, 0, QTableWidgetItem(submitter))
+        table.setItem(i, 1, QTableWidgetItem(document["issue_type"]))
+        table.setItem(i, 2, QTableWidgetItem(str(document["submitted_on"])))
+        table.setItem(i, 3, QTableWidgetItem(str(document["_id"])))
+        i += 1
+
+    if selection is not None:
+        table.setCurrentIndex(selection)
